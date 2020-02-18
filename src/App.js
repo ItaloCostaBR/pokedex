492ba975412facb72b245807ab12aeef5bf5c46e
Italo Lopes Costa <italo.costa@csktech.com.br>
2020-02-18 16:51:41 -0300

    feat: estruturando rotas do projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './assets/scss/style.scss';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+ } from 'react-router-dom';
+ import Header from './components/Header';
+ import Footer from './components/Footer';
+ import Home from './views/Home';
+ import Single from './views/Single';
 
 function App() {
   return (
     <div className="App">
-
+     <Router>
+        <Header />
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/:pokemon" component={Single} />
+        </Switch>
+        <Footer />
+    </Router>
     </div>
   );
 }
