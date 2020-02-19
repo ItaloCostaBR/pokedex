d58f58e60434b2397b7b6bb0fc481b039439617c
Italo Lopes Costa <italo.costa@csktech.com.br>
2020-02-19 10:58:23 -0300

    feat: estilizando header e footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './style.scss';
 
 const Footer = () => {
     return(
         <footer id="footer">
             <div className="container">
-                <h1 className="m-auto py-5">
-                    Pokedex - 2020
-                </h1>
+                <h5 className="m-auto py-3">
+                    © 2020 <a href="https://github.com/ythalocosta" target="_blank">Italo Costa</a> e colaboradores do <a href="https://pokeapi.co/" target="_blank">PokéAPI</a>.
+                </h5>
             </div>
         </footer>
     );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+import './style.scss';
 
 const Header = () => {
     return(
         <header id="header">
             <div className="container">
-                <div className="row">
-                    <div className="">
-                        <h1 className="m-auto py-5">
-                            Pokedex
-                        </h1>
-                    </div>
+                <div className="m-auto py-4">
+                    <Link to="/">
+                        <h1 style={{display: "none"}}>Pokedex</h1>
+                        <figure className="logo">
+                            <img src={logo} alt="Pokedex" />
+                        </figure>
+                    </Link>
                 </div>
             </div>
         </header>
