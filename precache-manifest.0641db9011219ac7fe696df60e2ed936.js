21e28625d4ae0e09d6aa348d76d8fefd5ec08153
Italo Lopes Costa <italo.costa@csktech.com.br>
2020-02-21 11:13:45 -0300

    Updates

---
+++ precache-manifest.0641db9011219ac7fe696df60e2ed936.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd19b8ab61b12a81207cace8cd59609",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "76261ce57e73d009981e",
+    "url": "/pokedex/static/css/2.efa40601.chunk.css"
+  },
+  {
+    "revision": "031ae21d4219a8f2ea45",
+    "url": "/pokedex/static/css/main.7c2735df.chunk.css"
+  },
+  {
+    "revision": "76261ce57e73d009981e",
+    "url": "/pokedex/static/js/2.c02ba8d5.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/pokedex/static/js/2.c02ba8d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031ae21d4219a8f2ea45",
+    "url": "/pokedex/static/js/main.6d149f4d.chunk.js"
+  },
+  {
+    "revision": "c2d3ab17bfc57c41f76f",
+    "url": "/pokedex/static/js/runtime-main.0a1f3015.js"
+  },
+  {
+    "revision": "bdcd20f5411ee5785889542d303ad4cb",
+    "url": "/pokedex/static/media/logo.bdcd20f5.png"
+  }
+]);
